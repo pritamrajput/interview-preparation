6abd6818fb227c15e3be73baa311427ba08c361c
Pritam chauhan <pritamchauhan669@gmail.com>
2024-12-21 20:58:34 +0530

    interview prep day 4

---
+++ javascript/array_part1.js
@@ -0,0 +1,32 @@
+// Arrays
+
+const myArray = [0, 1, 2, 3, 4, 5];
+
+const myHeros = ["shaktiman", "naagraj"];
+
+const myArray2 =  new Array(1, 2, 3, 4);
+
+console.log(myArray[1]);
+
+// Array methods
+// myArray.push(6);
+// myArray.push(7);
+// myArray.pop();
+
+// myArray.unshift(9);
+// myArray.shift();
+
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(3));
+
+const newArr =  myArray.join()
+
+console.log(myArray);
+console.log(newArr);
+console.log(typeof newArr);
+
+
+// slice ans splice
+
+
+
