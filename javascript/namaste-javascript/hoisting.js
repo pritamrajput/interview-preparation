c3bca6930bd204af004022ef92041d5b767402ef
Pritam chauhan <pritamchauhan669@gmail.com>
2024-12-30 14:11:27 +0530

    interview prep day 8

--- javascript/namaste-javascript/hoisting.js
+++ javascript/namaste-javascript/hoisting.js
@@ -1,9 +1,23 @@
-
-getName();
 console.log(x);
+console.log(getName);
+console.log(getName1);
+console.log(getName2);
+getName2();
 
 var x = 7;
 
 function getName(){
     console.log('Namaste javascript');
 }
+
+var getName2 = function(){
+    console.log('Namaste javascript rom getName2');
+}
+
+var getName3 = () => {
+    console.log('Namaste javascript rom getName2');
+}
+
+// getName();
+// console.log(x);
+// console.log(getName);
